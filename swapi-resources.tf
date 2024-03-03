6a4f6a963c513ddbf37c5a57934ea9b590b44722
Rodolpho Alves <rodolpho.castro.a+macosx@gmail.com>
2024-03-03 14:30:00 -0300

    feat: import all swapi resources

---
+++ swapi-resources.tf
@@ -0,0 +1,53 @@
+// tags for all resources within the platform-tools group
+locals {
+  swapi_tags = {
+    environment = "production"
+    project     = "swapi"
+  }
+}
+
+// terraform import azurerm_app_service_plan.swapi-asp /subscriptions/76bdb027-5c55-41d3-8ac4-43e2e2717ac6/resourceGroups/swapi/providers/Microsoft.Web/serverFarms/ASP-swapi-97a8
+// ASP-swapi-97a8 App Service Plan
+resource "azurerm_app_service_plan" "swapi-asp" {
+  name                = "ASP-swapi-97a8"
+  location            = azurerm_resource_group.swapi.location
+  resource_group_name = azurerm_resource_group.swapi.name
+  kind                = "Linux"
+  reserved            = true
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+  tags = local.swapi_tags
+}
+
+// terraform import azurerm_app_service.swapi_deno /subscriptions/76bdb027-5c55-41d3-8ac4-43e2e2717ac6/resourceGroups/swapi/providers/Microsoft.Web/sites/swapi-deno
+resource "azurerm_app_service" "swapi_deno" {
+  name                = "swapi-deno"
+  location            = azurerm_resource_group.swapi.location
+  resource_group_name = azurerm_resource_group.swapi.name
+  app_service_plan_id = azurerm_app_service_plan.swapi-asp.id
+  https_only          = true
+
+  site_config {
+    default_documents = [
+      "Default.htm",
+      "Default.html",
+      "Default.asp",
+      "index.htm",
+      "index.html",
+      "iisstart.htm",
+      "default.aspx",
+      "index.php",
+      "hostingstart.html",
+    ]
+  }
+
+  app_settings = {
+    "DOCKER_ENABLE_CI"                    = "true"
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://index.docker.io"
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+
+  tags = local.swapi_tags
+}
