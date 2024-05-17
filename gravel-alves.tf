e220022ae354c806c18fa8fac7b93bed59c908b4
Rodolpho Alves <rodolpho.castro.a+macosx@gmail.com>
2024-05-17 19:48:01 -0300

    chore: dont use identities on free tier of static web apps

--- gravel-alves.tf
+++ gravel-alves.tf
@@ -15,3 +15,20 @@ resource "azurerm_static_web_app" "gravel-alves" {
   location            = azurerm_resource_group.gravel-alves.location
   tags                = local.gravel_tags
 }
+
+// CName that maps from gravel-alves.com.br to the static web app host-name
+resource "azurerm_dns_cname_record" "gravel-alves-com-br" {
+  name                = "www"
+  zone_name           = azurerm_dns_zone.gravel-alves-com-br-dns.name
+  resource_group_name = azurerm_resource_group.platform-tools.name
+  ttl                 = 300
+  record              = azurerm_static_web_app.gravel-alves.default_host_name
+  tags                = local.platform_tags
+}
+
+# setting up the custom domain for the static web app
+resource "azurerm_static_web_app_custom_domain" "gravel-alves" {
+  static_web_app_id = azurerm_static_web_app.gravel-alves.id
+  domain_name       = "${azurerm_dns_cname_record.gravel-alves-com-br.name}.${azurerm_dns_cname_record.gravel-alves-com-br.zone_name}"
+  validation_type   = "cname-delegation"
+}
