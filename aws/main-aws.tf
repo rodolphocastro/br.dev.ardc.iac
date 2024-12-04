47896a5e4b532f09352a5bbadcfdbbcc20e69ede
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2024-12-04 18:18:18 -0300

    Feat/aws (#46)
    * refactor: move azure resources to theur own directory
    * ci: adjust pre-commit hook
    * ci: adjust PR check for Azure
    * ci: adjust PR check for Azure
    * ci: plan while checking Azure PRs
    * ci: plan while checking Azure PRs but using local state
    * ci: comment on Azure PRs
    * fix: grant write permission on PRs
    * chore: adjust wording
    * ci: deploy azure changes
    * chore: create an editorconfig
    * feat: create an AWS budget
    * ci: validate aws resources on pre-commit
    * ci: validate aws resources during PRs
    * fix: typo on AWS validation
    * ci: only sonar scan on terraform changes
    * feat: use an s3 bucket as aws backend
    * feat: add AWS deployment workflow with Terraform setup
    * chore: add YAML file formatting rules to .editorconfig
    * ci: refactor PR comment into its own job
    * fix: typos and wrong variables when commenting the PR
    
    ---------
    
    Signed-off-by: Rodolpho Alves <rodolpho.castro.a@gmail.com>

---
+++ aws/main-aws.tf
@@ -0,0 +1,33 @@
+# this folder will use AWS provider
+terraform {
+  # using AWS default provider for terraform
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  # backend for storing tfstate
+  backend "s3" {
+    bucket = "ardc-tf-bucket"
+    key    = "tf"
+    region = "us-east-2"
+  }
+}
+
+# default the provider to us-east-2
+provider "aws" {
+  region = "us-east-2"
+}
+
+# create a budget for the account, monthly U$10
+resource "aws_budgets_budget" "aws-budget" {
+  name              = "ardc-aws-budget"
+  budget_type       = "COST"
+  limit_amount      = 10
+  limit_unit        = "USD"
+  time_period_end   = "2087-06-15_00:00"
+  time_period_start = "2024-12-01_00:00"
+  time_unit         = "MONTHLY"
+}
